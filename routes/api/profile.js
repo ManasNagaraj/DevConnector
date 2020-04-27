c577199bf3f423a6355260c3b8715d5d48a9f76a
ManasNagaraj <48147533+ManasNagaraj@users.noreply.github.com>
2020-04-27 19:13:30 +0530

    commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const { check, validatorResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-//@route GET api/profile
-//@desc  Test route
-//@acess Public
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+//@route GET api/profile/me
+//@desc  Get current users profile
+//@acess Private
 
-router.get('/', (req, res) => res.send('profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'the profile does not exist' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(201).send('authenication failure');
+  }
+});
+
+//@route POST api/profile
+//@desc  create or update a users profile
+//@acess Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is reuired').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty,
+    ],
+  ],
+  async (req, res) => {
+    const errors = validatorResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
