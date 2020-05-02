edfb7be7ae1e6ebc980c617756a0a5dccf209fb6
ManasNagaraj <48147533+ManasNagaraj@users.noreply.github.com>
2020-05-03 00:10:37 +0530

    profile.js update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { check, validatorResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
+
 const auth = require('../../middleware/auth');
 
 const Profile = require('../../models/Profile');
@@ -34,16 +35,63 @@ router.post(
   [
     auth,
     [
-      check('status', 'Status is reuired').not().isEmpty(),
-      check('skills', 'Skills is required').not().isEmpty,
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
     ],
   ],
   async (req, res) => {
-    const errors = validatorResult(req);
-    if (!error.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+
+      res.status(400).json({ errors: errors.array() });
+      return;
     }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //builf profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+    console.log(profileFields.skills);
   }
 );
+
+//@route GET api/profile
+//@desc  Get all profiles
+//@acess Private
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
